45429d8c598fce1cf7068a431b4fc5723bd834e3
Raadwan Masum <piraadwan@gmail.com>
2020-09-05 09:21:00 -0700

    none of this worked

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use cursive::theme::ColorStyle;
 use cursive::views::TextView;
 
 fn main() {
     let mut siv = cursive::default();
+
+    siv.set_theme(theme: theme::Theme)
 
     siv.add_global_callback('q', |s| s.quit());
 
