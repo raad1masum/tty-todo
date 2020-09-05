9d756ec5b48b705b7bf30b70c2db7d2702dea482
Raadwan Masum <piraadwan@gmail.com>
2020-09-05 10:35:16 -0700

    get rid of config

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,10 @@ fn main() {
     let _matches = App::new("tty-todo")
         .version("1.0")
         .author("Raadwan Masum <piraadwan@gmail.com>")
-        .arg(Arg::with_name("config")
-            .short("c")
-            .long("config")
-            .value_name("FILE")
-            .help("Sets a custom config file")
-            .takes_value(true))
         .arg(Arg::with_name("INPUT")
             .help("Sets the input file to use")
             .required(true)
             .index(1))
-        .arg(Arg::with_name("v")
-            .short("v")
-            .multiple(true)
-            .help("Sets the level of verbosity"))
         .get_matches();
     println!("Hello, World!");
 }
