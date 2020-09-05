62c743e90b46acc3a5ca8f9057bd9251ecf8d1f7
Raadwan Masum <piraadwan@gmail.com>
2020-09-05 09:45:43 -0700

    get rid of cursive because it sucks

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-use cursive::theme::ColorStyle;
-use cursive::views::TextView;
-
 fn main() {
-    let mut siv = cursive::default();
-
-    siv.set_theme(theme: theme::Theme)
-
-    siv.add_global_callback('q', |s| s.quit());
-
-    siv.add_layer(TextView::new("Hello, world!"));
-
-    siv.run();
+    println!("Hello, World!");
 }
